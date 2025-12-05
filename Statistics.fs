c717ae0acaac448f3fd334d7f08fe4c04da23c4a
omnia <130509598+Omniawaleed@users.noreply.github.com>
2025-12-05 21:54:59 +0200

    Add files via upload

--- Statistics.fs
+++ Statistics.fs
@@ -1,32 +1,48 @@
-// Omnia Waleed
+namespace StudentManagement
 
-// Example Code:
-//  module Statistics =
-//  let studentAverage (s : Student) : float option =
-//  GradeCalc.averageScore s.Grades
-//  let classStats (passThreshold : float) (students : Student list) : ClassStats =
-//  let avgs =
-//  students
-//  |> List.choose (fun s -> studentAverage s)
-//  let highest =
-//  match avgs with
-//  | [] -> None
-//  | _ -> Some (List.max avgs)
-//  let lowest =
-//  match avgs with
-//  | [] -> None
-//  | _ -> Some (List.min avgs)
-//  let passCount =
-//  students
-//  |> List.filter (fun s -> GradeCalc.isPassing passThreshold s.Grades)
-//  |> List.length
-//  let total = students |> List.length
-//  let passRate =
-//  if total = 0 then None
-//  else Some (float passCount / float total)
-//  {
-//      StudentCount = total
-//      HighestAvg= highest
-//      LowestAvg= lowest
-//      PassRate= passRate
-//  }
+type ClassStats = {
+    StudentCount: int
+    HighestAvg: float option
+    LowestAvg: float option
+    PassRate: float option
+}
+module Statistics =
+
+    let studentAverage (student: Student) : float option =
+        if student.Grades.IsEmpty then None
+        else
+            let grades = student.Grades |> Map.toList |> List.map snd
+            Some (List.average grades)
+
+  
+    let isPassing (passThreshold: float) (grades: Map<string, float>) =
+        if grades.IsEmpty then false
+        else
+            let avg = grades |> Map.toList |> List.map snd |> List.average
+            avg >= passThreshold
+
+    let classStats (passThreshold: float) (students: Student list) : ClassStats =
+        let avgs =
+            students
+            |> List.choose studentAverage 
+
+       
+        let highest = if List.isEmpty avgs then None else Some (List.max avgs)
+        let lowest  = if List.isEmpty avgs then None else Some (List.min avgs)
+
+        let total = students.Length  
+        let passCount =
+            students
+            |> List.filter (fun s -> isPassing passThreshold s.Grades)
+            |> List.length
+
+        let passRate =
+            if total = 0 then None
+            else Some (float passCount / float total)
+
+        {
+            StudentCount = total
+            HighestAvg = highest
+            LowestAvg = lowest
+            PassRate = passRate
+        }
